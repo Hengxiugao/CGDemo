5bd31b5e83ec7226359840567ef3c12c122b6335
Hengxiugao <hengxiugao_us@yahoo.com>
2014-10-25 14:39:19 -0500

    Creates the GraphicsDemo object.

---
+++ displayGraphicsDemo.java
@@ -0,0 +1,11 @@
+// displayGraphicsDemo:
+//				Creates the GraphicsDemo object.
+
+public class displayGraphicsDemo
+{
+	public static void main(String[] args)
+	{
+		DemoDisplay myDemo = new DemoDisplay();
+	}
+}
+
