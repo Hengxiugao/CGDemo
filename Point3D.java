ab1e5223346452edc418b5e8b58a162c257009c5
Hengxiugao <hengxiugao_us@yahoo.com>
2014-10-25 14:45:34 -0500

    Representation of a point in 3D space.

---
+++ Point3D.java
@@ -0,0 +1,33 @@
+// Point3D.java: Representation of a point in 3D space.
+
+/* CGDemo is a companion of the textbook
+
+L. Ammeraal and K. Zhang, Computer Graphics for Java Programmers, 
+2nd Edition, Wiley, 2006.
+
+Copyright (C) 2006  Janis Schubert, Kang Zhang, Leen Ammeraal 
+
+This program is free software; you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as 
+published by the Free Software Foundation; either version 2 of 
+the License, or (at your option) any later version. 
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.  
+
+You should have received a copy of the GNU General Public 
+License along with this program; if not, write to 
+the Free Software Foundation, Inc., 51 Franklin Street, 
+Fifth Floor, Boston, MA  02110-1301, USA. 
+*/
+
+class Point3D
+{  float x, y, z;
+   Point3D(double x, double y, double z)
+   {  this.x = (float)x;
+      this.y = (float)y;
+      this.z = (float)z;
+   }
+}
